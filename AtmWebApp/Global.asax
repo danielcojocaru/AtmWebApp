ee31cafb773f2465995147e4101a757030e7a976
Daniel Cojocaru <daniel.cojocaru.ing@gmail.com>
2018-07-21 22:01:14 +0200

    Initial commit

---
+++ AtmWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AtmWebApp.MvcApplication" Language="C#" %>
---
+++ AtmWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AtmWebApp.Startup))]
+namespace AtmWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
